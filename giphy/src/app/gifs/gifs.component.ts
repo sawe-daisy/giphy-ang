9377be151aff00af05118076b91286deae9348a7
sawe-daisy <deesawe16@gmail.com>
2020-10-01 12:38:23 +0300

    linking

--- giphy/src/app/gifs/gifs.component.ts
+++ giphy/src/app/gifs/gifs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Giphy } from '../giphy';
+import { GiphyService } from '../giphy.service';
 
 @Component({
   selector: 'app-gifs',
@@ -8,9 +9,16 @@ import { Giphy } from '../giphy';
 })
 export class GifsComponent implements OnInit {
 
-  constructor() { }
+  constructor( private giphyService: GiphyService) { }
 
   ngOnInit(): void {
+    this.giphyService.gifRequest()
+    .subscribe((res: Response) => {
+      for (let i = 0; i < res[ 'data' ].length; i++){
+        
+        // console.log([res['data'][i]['images']['original']['url']]);
+      }
+    });
   }
 
 }
