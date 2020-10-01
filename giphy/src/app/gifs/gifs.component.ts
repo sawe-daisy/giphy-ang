c87d783bddaf6d3bd8404e640a5665943942625d
sawe-daisy <deesawe16@gmail.com>
2020-10-01 16:03:16 +0300

    display of gif images

--- giphy/src/app/gifs/gifs.component.ts
+++ giphy/src/app/gifs/gifs.component.ts
@@ -8,15 +8,21 @@ import { GiphyService } from '../giphy.service';
   styleUrls: ['./gifs.component.css']
 })
 export class GifsComponent implements OnInit {
+  gif: Giphy;
+
 
   constructor( private giphyService: GiphyService) { }
+  gifs = [];
 
   ngOnInit(): void {
     this.giphyService.gifRequest()
     .subscribe((res: Response) => {
       for (let i = 0; i < res[ 'data' ].length; i++){
-        
-        // console.log([res['data'][i]['images']['original']['url']]);
+        let gifTitle = res['data'][i]['title'];
+        let gifUname = res['data'][i]['username'];
+        let gifImage = res['data'][i]['images']['original']['url'];
+        let  giphy = new Giphy(gifTitle, gifUname, gifImage);
+        this.gifs.push(giphy);
       }
     });
   }
